7139360347ed4801da3f7c5bc290369e6f6f943a
CNLHC <cn_lhc@qq.com>
2019-11-08 18:02:53 +0800

    dev server side hot-reloading

--- packages/jacinth/src/command/dev.ts
+++ packages/jacinth/src/command/dev.ts
@@ -1,6 +1,10 @@
 import chokidar from 'chokidar'
 import preCheck from '../util/check';
-import { unsafeGetServerDir } from '../util/path';
+import { getTscPath, getServerDir } from '../util/path';
+import { unwrap } from '../util/fp'
+import { spawn } from 'child_process'
+import path from 'path'
+import chalk from 'chalk';
 interface IArgs {
     port: number
     hostname: string
@@ -8,10 +12,24 @@ interface IArgs {
 
 export default async (args: IArgs) => {
     preCheck()
+    const cwd = process.cwd()
+    const tscPath = unwrap(await getTscPath(cwd))
+    const serverDir = unwrap(await getServerDir(cwd))
+    const serverBuildPath = path.resolve(process.cwd(), '.jacinth', 'build')
+    const serverConf = path.resolve(serverDir, 'tsconfig.json')
     const loadBFF = require('../server/index')
+
     await loadBFF(args)
     try {
-        chokidar.watch(await unsafeGetServerDir(process.cwd()))
+        const tscwatcher = spawn(tscPath, ['--watch', '--outDir',serverBuildPath, '--project', serverConf])
+
+        tscwatcher.stdout.on('data',
+            (chunk) => console.error(chalk.red(`[Server-Tsc]`), chunk.toString()))
+
+        tscwatcher.stderr.on('data',
+            (chunk) => console.error(chalk.red(`[Server-Tsc]`), chunk.toString()))
+
+        chokidar.watch(serverBuildPath)
             .on('all', (event, path) => {
                 console.log("reload", event, path)
                 loadBFF(args)
--- packages/jacinth/src/util/path.ts
+++ packages/jacinth/src/util/path.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import fs from 'fs'
-const getDir = async (cwd: string, ingredients: string[]) => {
+const getPath = async (cwd: string, ingredients: string[]) => {
     const dir = path.resolve(cwd, path.join(...ingredients))
     return new Promise<string | undefined>((res) => {
         fs.exists(dir, (exists) => res(exists ? dir : undefined))
@@ -8,14 +8,18 @@ const getDir = async (cwd: string, ingredients: string[]) => {
 
 }
 
-export const getPagesDir = async (cwd: string) => await getDir(cwd, ['src', 'pages'])
-export const getServerDir = async (cwd: string) => await getDir(cwd, ['server'])
+export const getPagesDir = async (cwd: string) => await getPath(cwd, ['src', 'pages'])
+export const getServerDir = async (cwd: string) => await getPath(cwd, ['server'])
+export const getTscPath = async (cwd: string) => await getPath(cwd,['node_modules','.bin','tsc'])
+
 
 export const unsafeGetPagesDir = async (cwd: string) =>
     (await getPagesDir(cwd)) as string
 
 export const unsafeGetServerDir = async (cwd: string) =>
     (await getServerDir(cwd)) as string
+
+
 
 
 

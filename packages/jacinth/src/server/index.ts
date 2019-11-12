bbe3aadae512ebe5a3e5d5b97e23855e14a59882
CNLHC <cn_lhc@qq.com>
2019-11-12 11:28:55 +0800

    extract  restapi load logic

--- packages/jacinth/src/server/index.ts
+++ packages/jacinth/src/server/index.ts
@@ -1,46 +1,30 @@
 #!/usr/bin/env node
-import fastify from "fastify"
-import chalk from "chalk"
-import { gatherFile } from '../util/path'
-import { getEnv } from '../env/index'
-import { logger } from "../util/logging"
+import fastify from "fastify";
+import chalk from "chalk";
+import { getEnv } from "../env/index";
+import { logger } from "../util/logging";
 
-let serverRunning = false
-let server = fastify({})
+let serverRunning = false;
+let server = fastify({});
 
 module.exports = async () => {
-  logger.debug("reload bff")
-  const env = getEnv()
-  const gatherPlugin = async () => await gatherFile(env.cacheDir, ['*.js'])
-  //if there already have one instance running, kill it and get a new one 
+  logger.debug("reload bff");
+  const env = getEnv();
+  //if there already have one instance running, kill it and get a new one
   if (serverRunning) {
-    server.close()
-    server = fastify({})
+    server.close();
+    server = fastify({});
   }
-  const plugins = await gatherPlugin()
-  plugins.forEach(e => {
-    delete require.cache[require.resolve(e)]
-    const mod = require(e)
-    const opt = {
-      prefix: "/api"
-    }
-    logger.debug(`register plugin at ${e}`)
-    if (typeof mod === 'function')
-      server.register(mod, opt)
-    else if (typeof mod.default === 'function')
-      server.register(mod.default, opt)
-    else
-      logger.error(`unknown plugin type at ${e}`)
-  })
-  server.register(require('./plugins/next'))
-  let address;
-  try {
-    address = await server.listen(env.port, env.host)
-  } catch (e) {
-    logger.error(`server listening error ${e}`)
-  }
-  console.log(chalk.green(`server available at ${address}`))
-  serverRunning = true
-  return serverRunning
-}
 
+  server.after(() => {
+    server.register(require("./loader/rest").default, {
+      cacheDir: env.cacheDir
+    });
+    server.register(require("./plugins/next"));
+  });
+
+  const address = await server.listen(env.port, env.host);
+  logger.info(chalk.green(`server available at ${address}`));
+  serverRunning = true;
+  return serverRunning;
+};
--- packages/jacinth/src/server/types/plugin.ts
+++ packages/jacinth/src/server/types/plugin.ts
@@ -1,12 +1,19 @@
-import * as http from 'http';
-import * as http2 from 'http2';
-import * as https from 'https';
-
+import * as http from "http";
+import * as http2 from "http2";
+import * as https from "https";
+import fastify from "fastify";
+import fp from "fastify-plugin";
 
 export type HttpServer =
   | http.Server
   | https.Server
   | http2.Http2Server
   | http2.Http2SecureServer;
-export type RawRequest = http.IncomingMessage
-export type RawResponse = http.ServerResponse
+export type RawRequest = http.IncomingMessage;
+export type RawResponse = http.ServerResponse;
+
+export type TPlugin<T extends {}> = (
+  app: fastify.FastifyInstance<HttpServer,RawRequest,RawResponse>,
+  _opts: T,
+  done: fp.nextCallback
+) =>void;
